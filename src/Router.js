bbe693b01f7f329626589aca86a8c106f4883893
RosalynS78 <rrcervania78@gmail.com>
2022-08-11 20:21:54 -0500

    working

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router'
+import {Routes, Route, Navigate } from 'react-router'
 import cookie from 'cookie'
 import Home from './components/Home'
 import About from './components/About'
@@ -9,17 +9,36 @@ import Login from './components/Login'
 // Write checkAuth function here
 // Check the cookies for a cookie called "loggedIn"
 
+const checkAuth = () => {
+    const cookies = cookie.parse(document.cookie)
+    return cookies["loggedIn"] ? true : false
+}
 
 // Write ProtectedRoute function here
 
+// does not take in nested routes
+const ProtectedRoute = ({component: Component, ...rest}) => {
+    // console.log(Component)
+    return (
+        // <Route
+        //     {...rest}
+            // render={(props) => 
+                checkAuth()
+                ? <Component {...rest} />
+                : <Navigate to="/login" />
+        //     }
+        // />    
+    )
+}
 
 const Router = () => {
     return (
         <Routes>
-            <Route path="/" element={<Home/>} />
+            
             <Route path="/login" element={<Login/>} />
-            <Route path="/about" element={<About/>} />
-            <Route path="/car/:id" element={<Car/>} />
+            <Route path="/about" element={<ProtectedRoute component={About}/>} />
+            <Route path="/" element={<ProtectedRoute component={Home}/>} />
+            <Route path="/car/:id" element={<ProtectedRoute component={Car}/>} />
         </Routes>
     );
 };
