8a55fa68673cb05ac99e663fb263921f95d75d40
Ximilis <92741358+maxhomsi@users.noreply.github.com>
2022-08-15 18:44:32 -0500

    tentando entender

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,46 @@
+
+/* 
+    Write code for React Router here
+Import the Home and About components from 
+the component folder. Assign the default
+path "/" to the Home component and "/about"
+to the About component.
+Remember to export the component at the end
+*/
+
+
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router'
+// import { Routes, Route, Navigate } from 'react-router' 
+// Perguntar pro RObert pq ele mudou e tirou o navigate
+import { Routes, Route} from 'react-router' 
+import { Navigate } from "react-router-dom"
+
 import cookie from 'cookie'
 import Home from './components/Home'
 import About from './components/About'
 import Car from './components/Car'
 import Login from './components/Login'
 
-// Write checkAuth function here
+// Write checkAuth function here COMECAR A CHECAR 
+
+
+const checkAuth = () => {
+    // return true
+    const cookies = cookie.parse(document.cookie);
+    return cookies["loggedIn"] ? true : false;
+  };
+
 // Check the cookies for a cookie called "loggedIn"
 
+const ProtectedRoute = (props) => {
 
+    const { component: Component, ...rest } = props;
+  
+    return (
+      checkAuth() === true ? ( <Component {...rest} /> ) : ( <Navigate to="/" /> )
+    );
+  };
+  
 // Write ProtectedRoute function here
 
 
